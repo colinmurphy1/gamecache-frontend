9981d430d473879aa00972e87c83f2514a76459a
Colin Murphy <colin@colinmurphy.me>
2022-02-13 13:54:04 -0600

    Add user profile route

--- src/routes/UserProfile.svelte
+++ src/routes/UserProfile.svelte
@@ -10,14 +10,22 @@
   import getProfile from "../api/profile/getProfile.js";
   import getProfileGames from "../api/profile/getProfileGames.js";
 
+  // Load components
+  import Page from "../components/Page.svelte";
+  import PageTitle from "../components/headings/PageTitle.svelte";
+  import Card from "../components/card/Card.svelte";
+  import CardTitle from "../components/card/CardTitle.svelte";
+  import CardRow from "../components/card/CardRow.svelte";
+  import CardBody from "../components/card/CardBody.svelte";
   import GameList from '../components/GameList.svelte';
+  import Alert from "../components/Alert.svelte";
+  
 
 
   // Pass in parameters from App.svelte
   export let params;
 
   let username = params.username;
-
 
   let userProfile;
   let userProfileGames;
@@ -33,9 +41,6 @@
       console.log('Could not load profile: ', error);
       return false;
     }
-    /*finally {
-      console.log(userProfile);
-    }*/
 
     // Load Games
     try {
@@ -45,9 +50,6 @@
       console.log('Could not load game collection: ', error);
       return false;
     }
-    finally {
-      console.log(userProfileGames);
-    }
 
     gamesOwned = userProfileGames.length;
   });
@@ -56,81 +58,56 @@
   
 </script>
 
-{#if userProfile}
+<Page>
+  {#if userProfile}
 
-  <h1 class="text-4xl my-5 font-semibold px-4">
-    {userProfile.username}'s Profile
-  </h1>
+  <PageTitle title={userProfile.username} />
+
+
+  <!-- USER BADGES -->
+  <div class="flex flex-row space-x-2">
+    <!-- User online -->
+    {#if userProfile.online}
+    <div class="px-2 py-0.5 rounded text-sm bg-green-100 text-green-900 font-semibold">
+      <Icon data={circle} class="inline-block align-middle"/> Online
+    </div>
+    {/if}
+    <!-- User online END -->
+
+    <!-- User Admin -->
+    {#if userProfile.admin}
+    <div class="px-2 py-0.5 rounded text-sm bg-blue-100 text-blue-900 font-semibold">
+      <Icon data="{shield}" class="inline-block align-middle" /> Site Admin 
+    </div>
+    {/if}
+    <!-- User Admin END -->
+  </div>
+  <!-- USER BADGES END -->
+
+
 
   <!-- Main profile grid -->
   <div class="grid grid-cols-1 lg:grid-cols-3">
-
     <!-- User Stats / Bio -->
-    <div class="col-span-1 px-4">
-      <div class="shadow bg-gray-100 my-2 border border-gray-400">
-        <h2 class="border-b border-gray-400 text-center font-semibold bg-gray-200 text-gray-800 p-0.5 text-lg">
-          Stats
-        </h2>
-        <!-- USER TAGS -->
-        <div class="p-1 border-b border-gray-400 flex flex-row space-x-1">
-          <!-- User online -->
-          {#if userProfile.online}
-          <div class="px-2 py-0.5 rounded text-sm bg-green-100 text-green-900 font-semibold">
-            <Icon data={circle} /> Online
-          </div>
-          {/if}
-          <!-- User online END -->
-  
-          <!-- User Admin -->
-          {#if userProfile.admin}
-          <div class="px-2 py-0.5 rounded text-sm bg-blue-100 text-blue-900 font-semibold">
-            <Icon data="{shield}" /> Site Admin 
-          </div>
-          {/if}
-          <!-- User Admin END -->
-        </div>
-        <!-- USER TAGS END -->
-  
-        <!-- DATE JOINED -->
-        <div class="grid grid-cols-2 p-1 border-b border-gray-400">
-          <div class="text-gray-800 font-semibold">
-            Date joined:
-          </div>
-          <div>
-            {userProfile.dateJoined}
-          </div>
-        </div>
-        <!-- DATE JOINED END-->
-  
-        <!-- GAMES OWNED -->
-        <div class="grid grid-cols-2 p-1 border-gray-400">
-          <div class="text-gray-800 font-semibold">
-            Games in collection:
-          </div>
-          <div>
-            {gamesOwned}
-          </div>
-        </div>
-      </div>
-      <!-- GAMES OWNED END-->
-
-      <!-- USER BIO  -->
+    <div class="col-span-1 lg:pr-2">
+      <Card>
+        <CardTitle title="Stats" />
+        <CardRow key="Date joined:" value={userProfile.dateJoined} />
+        <CardRow key="Games in collection:" value={gamesOwned} />
+      </Card>
       {#if userProfile.bio}
-      <div class="shadow bg-gray-100 my-2 border border-gray-400">
-        <h2 class="border-b border-gray-400 text-center font-semibold bg-gray-200 text-gray-800 p-0.5 text-lg">
-          Bio
-        </h2>
-        <div class="p-1">
+      <Card>
+        <CardTitle title="Bio" />
+        <CardBody>
           <!-- Render markdown. TODO: MAKE SURE THIS IS SANITIZED!!! -->
           {@html marked.parse(userProfile.bio)}
-        </div>
-      </div>
+        </CardBody>
+      </Card>
       {/if}
-      <!-- USER BIO END -->
     </div>
 
     <!-- GAME COLLECTION -->
-    <div class="col-span-2 px-4">
+    <div class="col-span-2 lg:pl-2">
       <div class="shadow my-2">
         <h2 class="border-t border-l border-r border-gray-400 text-center font-semibold bg-gray-200 text-gray-800 p-0.5 text-lg">
           Games in collection
@@ -166,6 +143,7 @@
   </div>
 {:else}
 
-  <p>User does not exist</p>
+  <Alert title="Error" message="User does not exist." />
 
 {/if}
+</Page>
