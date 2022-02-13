acec36705cf4a9f42a7460a336cba751de118117
Colin Murphy <colin@colinmurphy.me>
2022-02-13 15:52:54 -0600

    Make site title smaller...

--- src/components/SiteTitle.svelte
+++ src/components/SiteTitle.svelte
@@ -2,7 +2,7 @@
   export let title = "Gamecache";
 </script>
 
-<div class="font-semibold text-center text-xl md:text-6xl"> <!--bg-indigo-300 sm:text-xl border-indigo-400 border-b-->
+<div class="font-semibold text-center text-xl md:text-4xl"> <!--bg-indigo-300 sm:text-xl border-indigo-400 border-b-->
   <a href="/" class="text-indigo-900 block py-4 hover:underline">
     {title}
   </a>
